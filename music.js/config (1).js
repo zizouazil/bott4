01c607ea8ddc7bd77b860dd13f59b743860e6913
mraco <36960271+mraco@users.noreply.github.com>
2018-04-29 21:15:56 +0100

    Delete config (1).js

--- music.js/config (1).js
+++
@@ -1,5 +0,0 @@
-exports.TOKEN = 'NDE0NDAzNzc4NDY1MjM0OTU0.DX2lqg.wsc26uXBdQBtgu7_nNu7oEOv-KY'; // ya wr3 la tns5
-
-exports.PREFIX = '*';
-
-exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
